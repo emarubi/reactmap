2e0e5288e5c2d051e853531bc2c7e9eb2e4920c3
emarubi <69473685+emarubi@users.noreply.github.com>
2021-03-04 10:08:37 +0100

    Reinstall cra
    yarn create react-app my-app
    in a new local directory

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Map from "../Map/Map";
+
+function App() {
+  return  <Map regionCoord={[48.864716, 2.349014]} regionName="Paris" />;
+
+}
+
+export default App;
---
+++ src/components/Map/constants.js
@@ -0,0 +1,11 @@
+import L from "leaflet";
+
+const icon = L.icon({
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+  iconUrl:  "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+  shadowUrl: "../public/marker-shadow.png"
+});
+
+export default icon ;
